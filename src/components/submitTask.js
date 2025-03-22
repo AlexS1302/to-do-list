e84508cbd22198ddf5d2b4ea84839c3306447fdc
AlexS1302 <135150527+AlexS1302@users.noreply.github.com>
2025-03-22 14:23:29 +0000

    Implement card generation in JS instead of HTML

--- src/components/submitTask.js
+++ src/components/submitTask.js
@@ -1,15 +1,18 @@
 import { createTask } from "./taskCreator";
 import { cardConstruction } from "./taskCard";
 
-let task;
+let tasks = [];
 
 export function submitTaskHandler() {
     const taskModal = document.getElementById("add-task-modal");
 
     document.getElementById("submit-task").addEventListener("click", (event) => {
         event.preventDefault();
-        task = createTask();
+        const task = createTask();
         console.log("Task created:", task);
+
+        tasks.push(task);
+        console.log("All tasks:", tasks);
 
         cardConstruction(task);
 
@@ -20,5 +23,5 @@ export function submitTaskHandler() {
 }
 
 export function getTask() {
-    return task;
+    return tasks;
 }
--- src/components/taskCard.js
+++ src/components/taskCard.js
@@ -1,30 +1,81 @@
 
-// To be used in submitTaskHandler() in submitTask.js
 export function cardConstruction(task) {
-    // Verification
     if (!task) {
         console.error("No task available to display");
+        return;
     }
 
-    // Make card visible
-    const cardTemplate = document.getElementById("card-template");
-    cardTemplate.style.display = "block";
+    // Generate card
+    const taskContainer = document.getElementById("main");
 
-    let cardTitle = document.querySelector(".card-title");
+    const card = document.createElement("div");
+    card.setAttribute("id", "card");
+    taskContainer.appendChild(card);
+
+    // Card header
+    const cardHeader = document.createElement("div");
+    cardHeader.setAttribute("id", "card-header");
+    card.appendChild(cardHeader);
+
+    // Card header children
+    const cardProject = document.createElement("h4");
+    cardProject.classList.add("card-project");
+    cardProject.textContent = task.project;
+
+    const cardTitle = document.createElement("h3");
+    cardTitle.classList.add("card-title");
     cardTitle.textContent = task.title;
 
-    let cardDesc = document.querySelector(".card-desc");
+    const cardDesc = document.createElement("p");
+    cardDesc.classList.add("card-desc");
     cardDesc.textContent = task.desc;
 
-    let cardPriority = document.querySelector(".card-priority");
+    cardHeader.appendChild(cardProject);
+    cardHeader.appendChild(cardTitle);
+    cardHeader.appendChild(cardDesc);
+
+    // Card body
+    const cardBody = document.createElement("div");
+    cardBody.setAttribute("id", "card-body");
+    card.appendChild(cardBody);
+
+    //Card body children
+    const cardNotesHeading = document.createElement("h4");
+    cardNotesHeading.classList.add("card-notes-heading");
+
+    const cardNotes = document.createElement("div");
+    cardNotes.classList.add("card-notes");
+
+    const cardPriority = document.createElement("div");
+    cardPriority.classList.add("card-priority");
     cardPriority.textContent = task.priority;
 
-    let cardDueDate = document.querySelector(".card-duedate");
+    cardBody.appendChild(cardNotesHeading);
+    cardBody.appendChild(cardNotes);
+    cardBody.appendChild(cardPriority);
+
+    // Line
+    const line = document.createElement("hr");
+    line.classList.add("line");
+    card.appendChild(line);
+
+    // Card footer
+    const cardFooter = document.createElement("div");
+    cardFooter.setAttribute("id", "card-footer");
+    card.appendChild(cardFooter)
+
+    //Card footer children
+    const cardDueDate = document.createElement("div");
+    cardDueDate.classList.add("card-duedate");
     let dueDate = task.dueDate;
     cardDueDate.textContent = dueDate.toLocaleDateString();
+    
+    cardFooter.appendChild(cardDueDate);
+    
+    //Buttons
+    
 
-    let cardProject = document.querySelector(".card-project");
-    cardProject.textContent = task.project;
+    
 
     console.log("Card template updated successfully!");
 }
