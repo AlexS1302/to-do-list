353b401b8a1b9233b2ed2b4f4223833b9214f5b7
AlexS1302 <135150527+AlexS1302@users.noreply.github.com>
2025-03-27 15:36:51 +0000

    Implement add project modal

--- src/components/displayModal.js
+++ src/components/displayModal.js
@@ -1,18 +1,42 @@
 
-// Modal
-export function displayModal() {
+const closeModalBtns = document.querySelectorAll(".close-modal");
+
+// Task Modal
+export function displayTaskModal() {
     const taskForm = document.getElementById("add-task-form");
     const taskModal = document.getElementById("add-task-modal");
     const addTaskBtn = document.getElementById("add-task-btn");
-    const closeModalBtn = document.getElementById("close-modal");
+    const closeModalBtn = document.querySelector(".close-modal");
 
     addTaskBtn.addEventListener("click", () => {
         taskForm.reset();
         taskModal.showModal();
     });
 
-    closeModalBtn.addEventListener("click", () => {
-        taskModal.close();
-        taskForm.reset();
+    closeModalBtns.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            taskModal.close();
+            taskForm.reset();
+        });
     });
+}
+
+// Project Modal
+export function displayProjectModal() {
+    const projectForm = document.getElementById("add-project-form");
+    const projectModal = document.getElementById("add-project-modal");
+    const addProjectBtn = document.getElementById("add-project-btn");
+
+    addProjectBtn.addEventListener("click", () => {
+        projectForm.reset();
+        projectModal.showModal();
+    });
+
+    closeModalBtns.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            projectModal.close();
+            projectForm.reset();
+        });
+    });
+
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import 'modern-normalize/modern-normalize.css';
 import "./styles.css";
 //import { format } from "date-fns";
 
-import { displayModal } from './components/displayModal';
+import { displayTaskModal, displayProjectModal  } from './components/displayModal';
 import { submitTaskHandler } from './components/submitTask';
 import { handleButtonClicks } from './components/footerBtns';
 import { handleNavClicks } from './components/navDates';
 
-displayModal();
+displayTaskModal();
+displayProjectModal();
 submitTaskHandler();
 handleButtonClicks();
 handleNavClicks();
